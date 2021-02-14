c16dd2ee63da978df610421e0b6016cc9b129431
GYANAM-DEVELOPER <74140267+GYANAM-ui@users.noreply.github.com>
2021-02-14 15:15:17 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,4 @@
+function clickAlert()
+{
+    window.alert("Beginning in the 6th century BC with the Pythagoreans, with Greek mathematics the Ancient Greeks began a systematic study of mathematics as a subject in its own right. Around 300 BC, Euclid introduced the axiomatic method still used in mathematics today, consisting of definition, axiom, theorem, and proof.")
+}
